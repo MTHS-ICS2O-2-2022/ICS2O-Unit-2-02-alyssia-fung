ca7514cc4a59954c27f3203dc816775e7f23ee26
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-03-10 17:43:33 +0000

    added in template

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  document.getElementById("area").innerHTML = "<p>area= " + (5 * 3) + "</p>"
+  document.getElementById("perimeter").innerHTML = "<p>perimeter= + " +2* (5 + 3)"</p>" 
+}
