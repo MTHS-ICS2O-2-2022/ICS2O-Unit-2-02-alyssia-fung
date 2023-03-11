18cbd6a82d7bd99824868d93eeccc2603c0a084c
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-03-11 02:23:58 +0000

    fixed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Alyssiafung All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Alyssiafung
+// Created on: Mar 2030
 // This file contains the JS functions for index.html
 
 /**
  * Do basic math.
  */
 function doMathClicked() {
-  document.getElementById("area").innerHTML = "<p>area= " + (5 * 3) + "</p>";
-  document.getElementById("perimeter").innerHTML = "<p>perimeter= + " + 2*(5 + 3)"</p>" ;
+  document.getElementById("area").innerHTML = "<p>area= + " + (5 * 3) + " cm² </p>"
+  document.getElementById("perimeter").innerHTML = "<p>perimeter= + " + 2*(5 + 3) + "cm </p>"
 }
